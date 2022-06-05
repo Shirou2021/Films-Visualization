734d4ab82c80f1f0c862415ff7b978595fbeab20
Shirou2021 <shilicao@outlook.com>
2022-06-04 18:50:04 -0700

    done

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 # Films-Visualization
 
-This program aims to provide clear visualizations of the database. 
-The best way to visualize would be through different types of graph.
+This program aims to provide clear visualizations of the database via line plot, 
+heatmap, and bar graph. Furthermore, it will also serves as an intro practice for 
+data visualizations/analysis. 
 
-The csv file I created attracts top 100 films of all time, and they are 
+The csv file I created attracted top 100 films of all time, and they are 
 categorized via rating, year of release, and the overall gross earning. 
 
 More info about the csv file can be found on IDMb.com. 
 
 For testing, simply do python3 'name of the file'.py. Then the terminal or console
 will generated .png files, serve as visualizations.
+
+A quick side note: Due to the size of dataset is relatively small compared to others, 
+hence the pictures that are going to be generated might not look too fancy/good.
+
--- Source/films.py
+++ Source/films.py
@@ -1,9 +1,9 @@
-from turtle import up
 import pandas as pd
 import numpy as np
-import seaborn as sn
+import seaborn as sns
 import matplotlib.pyplot as plt
 import sys
+
 
 # create few visual graphs to best analyze the csv file.
 # ideas:
@@ -11,42 +11,52 @@ import sys
 # data frame
 df = pd.read_csv('Best_Films.csv', parse_dates = ["Year"], index_col = "Year")
 df.describe()
-# set the index column 
-#df = df.set_index('Year')
+
 #set the index columndf = df[(df[""])]
 df = df[(df["Gross_earning (in millions)"] >= df["Gross_earning (in millions)"].quantile(0.025))
  & (df["Gross_earning (in millions)"] <= df["Gross_earning (in millions)"].quantile(0.975))]
 
-# def heat_map():
-#     corr = df.corr(method = "pearson")
-#     fig = plt.subplots(figsize=(10,10))
-#     #sn.heatmap(corr, linewidth = 1, annot = True), mask = np.triu(corr))
-#     fig.savefig('heapmap.png')
-#     return 
+df2 = df[(df["Rating"] >= df["Rating"].quantile(0.025))
+ & (df["Rating"] <= df["Rating"].quantile(0.975))]
+#df = df.loc[df(df['Year'] >= 1900 & df['Year'] <= 2020)]
+
+
+def heat_map():
+    df_heat = df[(df["Gross_earning (in millions)"] >= df["Gross_earning (in millions)"].quantile(0.025))
+ & (df["Gross_earning (in millions)"] <= df["Gross_earning (in millions)"].quantile(0.975)) & (df["Rating"] >= df["Rating"].quantile(0.025))
+ & (df["Rating"] <= df["Rating"].quantile(0.975))]
+
+    correlation = df_heat.corr()
+    
+    mask = np.triu(correlation)
+    fig = plt.subplots(figsize=(7,7))
+    sns.heatmap(correlation, linewidth = 1, annot = True, square = True, mask = mask, cbar_kws = {'shrink' : .60})
+    title = 'Heatmap of Films'
+    plt.title(title, fontsize = 13)
+    plt.show()
+    return 
 
 def line_plot():
     fig, ax = plt.subplots(figsize=(12, 10))
+    
     ax.plot(df.index, df['Gross_earning (in millions)'], 'r', linewidth = 1)
-    plt.xlabel("Year", fontsize = 15)
+    
+    plt.xlabel("Year",  fontsize = 15)
     plt.ylabel("Revenue (in millions)", fontsize = 15)
     plt.title("Top 100 films of all time")
     fig.savefig('line_plot.png')
     return fig
     
 
-# def bar_graph():
-    # parsing and make a top 10 films of all time.
-    # df.loc[df['rating'] > 9.5] ...
-#     return  
+def bar_graph():
+    fig = plt.subplots(figsize=(10, 10))
+    sns.barplot(x = "Rating", y = "Films",data= df2.head(10),color= "red", alpha=0.5,label ="Rating")
+    plt.xlabel("Rating",  fontsize = 15)
+    plt.ylabel("Films", fontsize = 15)
+    plt.title("Top 10 films")
+    plt.show()
+    return 
 
-# Must finish by 6/4/22. 
-# So, plan:
-# 1. wake up
-# 2. breakfast.
-# 3. Quick exercise 
-# 4. Prepare for lunch items and start working on it. No nap torrmow.
-
-
+heat_map()
 line_plot()
-# bar_graph()
-# heat_map()
+bar_graph()
